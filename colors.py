8b58d476a46ccc98721717fba02c8ad3d9358a78
Eric Blevins <eblevins@kmilearning.com>
2021-05-02 21:41:21 -0400

    improved temp color gradient

--- colors.py
+++ colors.py
@@ -5,6 +5,8 @@ def temp(temp,min,max):
     shift = 0
     if (min < 0):
         shift = abs(min)
+    if ( min > 0 ):
+        shift = 0 - min
     temp = float(temp) + shift  # Shift the scale so it starts from 0
     if ( temp < 0 ):
         temp = 0  # If temp is less than min then set to 0
@@ -12,7 +14,7 @@ def temp(temp,min,max):
     if ( temp > max): #highest hue is 38c ~100f
         temp = max
     # Calculate the hue between 140(min) and 360(max)
-    hue = (240 + (120 * ( temp / (max + shift ) )))/ 360
+    hue = (240 - (240 * ( temp / (max + shift ) ))) / 360
     return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(hue,1,1))
 
 
